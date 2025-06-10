410f4617cf74463ebd0c8ad01b5c3a175749e450
Gustavo Azevedo <ghca12@gmail.com>
2025-06-10 19:56:08 -0300

    Limpando imports que não uso no Personagens.tsx

--- laura/src/components/Personagens/Personagens.tsx
+++ laura/src/components/Personagens/Personagens.tsx
@@ -2,11 +2,6 @@
 import { Box } from '@mui/material'
 import Personagem from './Personagem'
 import laura from '../../assets/laura.png'
-import hp from '../../assets/hp.png'
-import dica2 from '../../assets/hp.png'
-import gustavo from '../../assets/gustavo.png'
-import Title from '../Title/Title'
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 const Personagens = () => {
     const navigate = useNavigate()
